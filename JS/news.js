1bdedccf78692c7f30aeddfda216c0f8362aa2b6
yoav74 <yoav74@gmail.com>
2025-06-19 19:49:40 +0300

    continued work on news

--- JS/news.js
+++ JS/news.js
@@ -3,7 +3,6 @@ let newsContainer;
 
 window.onload = () => {
   newsContainer = document.getElementById("news-section");
-  let newsArr = [];
   fetch(API_BASE + "/news")
     .then((response) => response.json())
     .then((response) => {
@@ -13,15 +12,24 @@ window.onload = () => {
 };
 
 renderNews = (newsArr) => {
-    // console.log("entered here");
-    console.log(newsArr);
+  // console.log("entered here");
+  console.log(newsArr);
   for (let news of newsArr) {
     const newDiv = document.createElement("section");
     newDiv.classList.add("news-container");
+    const imgDir = API_BASE + "/" + news.img_path;
+    console.log(news.img_path);
+    console.log(imgDir);
     newDiv.innerHTML = `
-    <h2>${news.title}</h2>
-    <p>${news.description}</p>
-    `
+    <section class="text">
+        <h2>${news.title}</h2>
+        <p>${news.description}</p>
+    </section>
+    <section class="news-img-container">
+        <img src=${imgDir}>
+    </section>
+    
+    `;
     newsContainer.appendChild(newDiv);
   }
 };
