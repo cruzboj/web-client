746de3a052efb99c6feae94b4ce1631d16a38e24
daniel <168971595+cruzboj@users.noreply.github.com>
2025-07-09 19:09:15 +0300

    need to fix add card function

---
+++ JS/adminNav.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createNav();
+  });
+
+function createNav(){
+  const navbar = document.querySelector(".toolbar");
+  navbar.innerHTML = `
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <h4 class="text-center">
+          <img src="{url}" alt="Profile" class="rounded-circle mb-3" width="80" height="80">
+      </h4>
+      <a href="admin.html">Dashboard</a>
+      <a href="adminData.html">User Data</a>
+      <a href="#">Manage Packs</a>
+      <a href="index.html">Back to Site</a>
+      <hr class="bg-light">
+      <a href="#">Settings</a>
+      <a href="#">Logout</a>
+    </div>
+  `;
+}
